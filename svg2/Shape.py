65002caf3d40eb42a1b918b89a1e35cec571e3d8
mark-summerfield <mark@qtrac.eu>
2021-10-08 12:43:13 +0100

    some docs

--- svg2/Shape.py
+++ svg2/Shape.py
@@ -8,6 +8,8 @@ from . import AbstractShape
 class Line(AbstractShape.AbstractStroke):
 
     def __init__(self, x1, y1, x2, y2, *, stroke=None):
+        '''The stroke can be a Stroke, Color, or color string (e.g., 'red',
+        '#ABC123').'''
         super().__init__(stroke)
         self.x1 = x1
         self.y1 = y1
@@ -24,6 +26,8 @@ class Line(AbstractShape.AbstractStroke):
 class Rect(AbstractShape.AbstractPositionStrokeFill):
 
     def __init__(self, x, y, *, width, height, stroke=None, fill=None):
+        '''The stroke can be a Stroke, Color, or color string (e.g., 'red',
+        '#ABC123'). The fill can be a Fill, Color, or color string.'''
         super().__init__(x, y, stroke, fill)
         self.width = width
         self.height = height
@@ -38,6 +42,8 @@ class Rect(AbstractShape.AbstractPositionStrokeFill):
 class Circle(AbstractShape.AbstractPositionStrokeFill):
 
     def __init__(self, x, y, *, radius, stroke=None, fill=None):
+        '''The stroke can be a Stroke, Color, or color string (e.g., 'red',
+        '#ABC123'). The fill can be a Fill, Color, or color string.'''
         super().__init__(x, y, stroke, fill)
         self.radius = radius
 
@@ -51,7 +57,9 @@ class Circle(AbstractShape.AbstractPositionStrokeFill):
 class Ellipse(Circle):
 
     def __init__(self, x, y, *, xradius, yradius, stroke=None, fill=None):
-        '''`radius` is a synonym for `xradius`.'''
+        '''`radius` is a synonym for `xradius`.
+        The stroke can be a Stroke, Color, or color string (e.g., 'red',
+        '#ABC123'). The fill can be a Fill, Color, or color string.'''
         super().__init__(x, y, radius=xradius, stroke=stroke, fill=fill)
         self.yradius = yradius
 
@@ -77,5 +85,7 @@ class Ellipse(Circle):
 class Path(AbstractShape.AbstractStrokeFill):
 
     def __init__(self, *, stroke=None, fill=None):
+        '''The stroke can be a Stroke, Color, or color string (e.g., 'red',
+        '#ABC123'). The fill can be a Fill, Color, or color string.'''
         super().__init__(stroke, fill)
         self.d = [] # sequence of drawing commands
